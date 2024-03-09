957fe71a65cbb60e306299d7ac8a7d0d45a884a8
caothanh510 <hc.thanh510@gmail.com>
2024-03-09 21:01:33 +0700

    production config

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ threads min_threads_count, max_threads_count
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
   require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
   workers worker_count if worker_count > 1
 end
 
@@ -26,10 +26,26 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+if ENV["RAILS_ENV"] == "production"
+  # Specifies the number of `worker_timeout` seconds that Puma will wait before
+  # terminating a worker in production environments.
+  worker_timeout ENV.fetch("WORKER_TIMEOUT") { 60 }
+
+  # Set up socket location for nginx
+  bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+
+  # Logging
+  stdout_redirect "/var/log/puma/puma.log", "/var/log/puma/puma_err.log", true
+
+  # Set master PID and state locations
+  state_path "/var/run/puma/puma.state"
+  activate_control_app
+end
